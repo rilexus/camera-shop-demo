fee79089f51d156bab7e0772d434dbd71db3b730
rilexus <stanip@hotmail.de>
2022-09-10 21:35:11 +0200

    refactor(createContext): removed provider

--- Providers/utils/createContext.js
+++ Providers/utils/createContext.js
@@ -1,19 +1,14 @@
 import { createContext as reactCreateContext, useContext } from "react";
 
-const createContext = (initialState, name) => {
+const createContext = (initialState) => {
   const context = reactCreateContext(initialState);
 
-  const Provider = ({ children, value }) => {
-    return <context.Provider value={value}>{children}</context.Provider>;
+  return {
+    ...context,
+    useContext: () => {
+      return useContext(context);
+    },
   };
-
-  Provider.displayName = `${name || "ContextProvider"}`;
-
-  const use = () => {
-    // eslint-disable-next-line
-    return useContext(context);
-  };
-
-  return { ...context, Provider, useContext: use };
 };
+
 export default createContext;
